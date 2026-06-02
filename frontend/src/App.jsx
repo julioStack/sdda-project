be0a3e62902c3f86780c5d8e6a505dc0c755640c
JULIO GUDIÑO <julio.gudino1@utp.ac.pa>
2026-06-02 01:27:32 -0500

    Implementacion almacenamiento temporal de registros

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,95 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [nombre, setNombre] = useState("");
+  const [tipoPersona, setTipoPersona] = useState("Natural");
+  const [pais, setPais] = useState("Panamá");
+  const [actividad, setActividad] = useState("");
+
+  const [registros, setRegistros] = useState([]);
+
+  const registrarTercero = (e) => {
+    e.preventDefault();
+
+    const nuevoRegistro = {
+      nombre,
+      tipoPersona,
+      pais,
+      actividad,
+    };
+
+    setRegistros([...registros, nuevoRegistro]);
+
+    setNombre("");
+    setTipoPersona("Natural");
+    setPais("Panamá");
+    setActividad("");
+  };
+
   return (
     <div className="container">
       <h1>Sistema de Debida Diligencia Anticorrupción (SDDA)</h1>
 
-      <form className="formulario">
+      <form className="formulario" onSubmit={registrarTercero}>
         <label>Nombre del tercero</label>
-        <input type="text" placeholder="Ingrese el nombre" />
+        <input
+          type="text"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+          required
+        />
 
         <label>Tipo de persona</label>
-        <select>
+        <select
+          value={tipoPersona}
+          onChange={(e) => setTipoPersona(e.target.value)}
+        >
           <option>Natural</option>
           <option>Jurídica</option>
         </select>
 
         <label>País</label>
-        <select>
+        <select value={pais} onChange={(e) => setPais(e.target.value)}>
           <option>Panamá</option>
           <option>Costa Rica</option>
           <option>Colombia</option>
         </select>
 
         <label>Actividad económica</label>
-        <input type="text" placeholder="Ingrese la actividad" />
+        <input
+          type="text"
+          value={actividad}
+          onChange={(e) => setActividad(e.target.value)}
+          required
+        />
 
         <button type="submit">Registrar</button>
       </form>
+
+      <h2>Registros Realizados</h2>
+
+      <table border="1" width="100%">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tipo Persona</th>
+            <th>País</th>
+            <th>Actividad</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {registros.map((registro, index) => (
+            <tr key={index}>
+              <td>{registro.nombre}</td>
+              <td>{registro.tipoPersona}</td>
+              <td>{registro.pais}</td>
+              <td>{registro.actividad}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
